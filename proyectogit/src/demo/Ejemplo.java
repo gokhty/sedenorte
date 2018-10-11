2a736fd3e88ee5fa04cc863551fc99ba929f6409
gokhty <cibertron12345@gmail.com>
2018-10-10 20:57:33 -0500

    porella

---
+++ proyectogit/src/demo/Ejemplo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git hub");
+	}
+	public void mostrar(){
+		
+	}
+	public void mostrar2(){
+		System.out.println("");
+	}
+
+}
